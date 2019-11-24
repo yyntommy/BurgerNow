865da98813f90ae2d4c1abd05414c8106e16e946
Yuning (Tommy) Yang <yyntommy@gmail.com>
2019-11-24 01:23:17 -0500

    finished with routing

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://react-my-burger-b513d.firebaseio.com/'
+    baseURL: 'https://react-my-burger-b513d.firebaseio.com/'
 });
 
 export default instance;
